afb888980d9f48a28cc744f17b986d92bd73a62a
vaayusaini <vaayusaini@outlook.com>
2026-02-20 17:05:47 -0800

    feat: create fire mechanic

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#include "CSerialPort/SerialPort.h"
+#include "CSerialPort/SerialPortInfo.h"
 #include "detector/Detector.h"
 #include "videostream/CameraSource.h"
 #include "videostream/ImageViewer.h"
-#include <CSerialPort/SerialPort.h>
 #include <chrono>
+#include <iostream>
+#include <string>
 #include <thread>
+#include <unistd.h>
+#include <vector>
 
 int detect() {
   pd::CameraSource cam0(0);
@@ -66,4 +71,81 @@ int detect() {
   return 0;
 }
 
-int main() {}
+void writeAngleToMotor(itas109::CSerialPort &motor, int angle) {
+  const std::string packet = std::to_string(angle) + "\n";
+
+  const int size = static_cast<int>(packet.size());
+  motor.writeData(packet.data(), size);
+}
+
+void sleepFor(int ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
+
+void fireTriggerMotor(itas109::CSerialPort &motor) {
+  writeAngleToMotor(motor, 65);
+  sleepFor(1000);
+
+  writeAngleToMotor(motor, 35);
+}
+
+int arduino() {
+  using itas109::CSerialPort;
+
+  const std::vector<itas109::SerialPortInfo> ports = itas109::CSerialPortInfo::availablePortInfos();
+
+  if (ports.empty()) {
+    std::cerr << "No serial ports found.\n";
+    return -1;
+  }
+
+  for (int i = 0; i < ports.size(); i++) {
+    std::cout << "port: " << ports[i].portName << " id: " << ports[i].hardwareId << std::endl;
+  }
+
+  std::string portName;
+
+  std::cout << "enter the motor port name:";
+  std::cin >> portName;
+
+  itas109::CSerialPort motor(portName.c_str());
+  motor.setBaudRate(9600);
+  motor.open();
+
+  if (!motor.isOpen()) {
+    std::cout << "unable to connect to motor " << portName;
+    return -1;
+  }
+
+  sleepFor(100);
+
+  while (true) {
+    std::cout << "enter input:";
+
+    std::string input;
+    std::cin >> input;
+
+    if (input == "q") {
+      break;
+    }
+
+    fireTriggerMotor(motor);
+  }
+
+  // while (true) {
+  //   fireTriggerMotor(motor);
+  //   sleepFor(1000);
+
+  // for (int angle = 35; angle <= 85; angle++) {
+  //   writeAngleToMotor(motor, angle);
+  //   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  // }
+
+  // for (int angle = 85; angle >= 35; angle--) {
+  //   writeAngleToMotor(motor, angle);
+  //   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  // }
+  // }
+
+  return 0;
+}
+
+int main() { return arduino(); }
